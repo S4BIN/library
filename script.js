d2161238ba94c70ef1991975f8ef776705a933c0
Burlacu Ioan Sabin <118127389+S4BIN@users.noreply.github.com>
2023-04-11 23:01:05 +0300

    index.html and script.js

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 let myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-function addBookToLibrary() {}
+function render() {
+  let libraryBook = document.querySelector("library");
+  for (let i = 0; i < myLibrary.length; i++) {}
+}
 
-let newBookbtn = document.querySelector("#new0book-btn");
+function addBookToLibrary() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").value;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary / PushManager(newBook);
+  render();
+}
+
+let newBookbtn = document.querySelector("#new-book-btn");
 newBookbtn.addEventListener("click", function () {
-  let newBookForm = document.querySelector("#new-book-btn");
+  let newBookForm = document.querySelector("#new-book-form");
   newBookForm.style.display = "block";
 });
+
+document
+  .querySelector("#new-book-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    addBookToLibrary();
+  });
