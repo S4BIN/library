3bb86147a1cab296cbb4af504da90fdaf23d903b
Burlacu Ioan Sabin <118127389+S4BIN@users.noreply.github.com>
2023-04-12 12:37:39 +0300

    script.js

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ function Book(title, author, pages, read) {
 }
 
 function render() {
-  let libraryBook = document.querySelector("library");
-  for (let i = 0; i < myLibrary.length; i++) {}
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.setAttribute("class", "book-card");
+    bookEl.innerHTML = `
+      <div class="card-header">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="author">by ${book.author}</h5>
+      </div> 
+      <div class="card-body">
+        <p>${book.pages} pages</p>
+        <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+      </div>`;
+    libraryEl.appendChild(bookEl);
+  }
 }
 
 function addBookToLibrary() {
@@ -18,7 +33,7 @@ function addBookToLibrary() {
   let pages = document.querySelector("#pages").value;
   let read = document.querySelector("#read").value;
   let newBook = new Book(title, author, pages, read);
-  myLibrary / PushManager(newBook);
+  myLibrary.push(newBook);
   render();
 }
 
